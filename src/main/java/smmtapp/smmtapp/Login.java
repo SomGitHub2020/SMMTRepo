b7203f1cc78d049c2190631601d5b72ffb6fa2b1
SomGitHub2020 <61223781+SomGitHub2020@users.noreply.github.com>
2020-04-09 16:36:57 +0530

    9-APR-2020-6

--- src/main/java/smmtapp/smmtapp/Login.java
+++ src/main/java/smmtapp/smmtapp/Login.java
@@ -2,22 +2,50 @@ package smmtapp.smmtapp;
 
 public class Login {
 
-	  private long id;
-	  private String content;
+	private String ip;
+	private int port;
+	private String user;
+	private String password;
+	private String stringURL;
 
-	  public long getId() {
-	    return id;
-	  }
 
-	  public void setId(long id) {
-	    this.id = id;
-	  }
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
 
-	  public String getContent() {
-	    return content;
-	  }
+	public int getPort() {
+		return port;
+	}
 
-	  public void setContent(String content) {
-	    this.content = content;
-	  }
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getStringURL() {
+		return stringURL;
+	}
+
+	public void setStringURL(String stringURL) {
+		this.stringURL = "http://"+ ip + ":" + port + "/XMII/Illuminator?service=scheduler&mode=List&content-type=text/xml&IllumLoginName=" + user + "&IllumLoginPassword=" + password;;
+	}
+
 }
