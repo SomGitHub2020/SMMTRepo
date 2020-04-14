7a50f3b56e1f44669bf318d6a59620c5678abcbe
SomGitHub2020 <61223781+SomGitHub2020@users.noreply.github.com>
2020-04-14 16:36:21 +0530

    14-APR-2020-3

--- src/main/java/smmtapp/smmtapp/HomeController.java
+++ src/main/java/smmtapp/smmtapp/HomeController.java
@@ -27,7 +27,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class HomeController {
 
-	  @GetMapping("/login")
+	@GetMapping
+    public String index() {
+      return "redirect:/login";
+    }
+	
+	
+	@GetMapping("/login")
 	  public String loginForm(Model model) {
 	    model.addAttribute("login", new Login());
 	    return "login";
